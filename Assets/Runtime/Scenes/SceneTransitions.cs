1184cd1d04aa09f42fea4193d58ce477c62322b3
Ayslan_gamedev <ayslan.gamedev@gmail.com>
2023-06-18 19:39:27 -0300

    update
    +mudança de local do package

--- Assets/Runtime/Scenes/SceneTransitions.cs
+++ Assets/Runtime/Scenes/SceneTransitions.cs
@@ -1,18 +1,25 @@
+using CatCity;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneTransitions : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameManager manager;
+
+    private void Start()
     {
-        
+        manager = GameObject.Find(EditorVariables.GAME_MANAGER.ToString()).GetComponent<GameManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    /// <summary>
+    /// Load a scene with the load scene 
+    /// </summary>
+    /// <param name="scene">scene to load</param>
+    public void LoadNewScene(string scene)
     {
-        
+        SceneManager.LoadScene(EditorVariables.LoadScene.ToString());
+        //CurrentScene = scene;
     }
 }
