3cfc3efe5ab346985028b25a55510758eb2fd5de
Ayslan_gamedev <ayslan.gamedev@gmail.com>
2023-06-19 08:39:22 -0300

    refactor - files
    +formatação de arquivos modificada

--- Assets/Runtime/Settings/GameSettings.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-using CatCity;
-
-[CreateAssetMenu(fileName = "Game Settings", menuName = "Save/Game Settings")]
-public class GameSettings : ScriptableObject
-{
-    public MenuValues saveMenuValues;
-}
