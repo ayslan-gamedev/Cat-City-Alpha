b6278fc04df43601cbb281805f5358925a54cb44
Ayslan_gamedev <ayslan.gamedev@gmail.com>
2023-07-05 23:15:16 -0300

    FEAT - Menu System (incompleto)

--- Assets/Runtime/GameManager/GameManager.cs
+++ Assets/Runtime/GameManager/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Reflection;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 namespace CatCity
@@ -34,5 +36,47 @@ namespace CatCity
         {
             Debug.Log("AAAAAAAAAAAAAAAAAAA");
         }
+
+        #region GameSettings
+        private int m_quality;
+
+        /// <summary>
+        /// Current game quality
+        /// </summary>
+        public int GameQuality 
+        { 
+            get { return m_quality; }
+            set
+            {
+                if(value != m_quality)
+                {
+                    QualitySettings.SetQualityLevel(value);
+                    m_quality = value;
+                }
+                else
+                {
+                    Debug.LogFormat(value + "is not a valid Quality");
+                }
+            }
+        }
+
+        private int m_resolution;
+
+        public int GameResolution
+        {
+            get { return m_resolution; }
+            set
+            {
+                if(value != m_resolution)
+                {
+                    Resolution resolution = Screen.resolutions[value];
+                    Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+                    PlayerPrefs.SetInt("ResolutionIndex", m_resolution);
+                    m_resolution = value;
+                }
+            }
+        }
+
+        #endregion
     }
 }
---
+++ Assets/Runtime/Menus/Scripts/MenuSettings.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[AddComponentMenu("Cat City/MenuSettings")]
+[ExecuteInEditMode]
+public class MenuSettings : MonoBehaviour
+{
+
+    private int currentQualityIndex;
+    private int currentResolutionIndex;
+
+    private void Start()
+    {
+        // Armazena as configurações atuais
+        currentQualityIndex = QualitySettings.GetQualityLevel();
+        currentResolutionIndex = PlayerPrefs.GetInt("ResolutionIndex", Screen.resolutions.Length - 1);
+    }
+
+
+
+    #region VIDEO
+
+    public void SetResolution()
+    {
+
+    }
+
+    public void SetQuality()
+    {
+
+    }
+
+    private bool m_fullScreen;
+
+    /// <summary>
+    /// Set the game FullScreen
+    /// </summary>
+    public bool FullScreen 
+    {
+        get { return m_fullScreen; }
+        set
+        {
+            m_fullScreen = value;
+            Screen.fullScreen = value;
+        }
+    }
+    #endregion
+
+    #region AUDIO
+
+    /// <summary>
+    /// Play any sund in game
+    /// </summary>
+    public bool MuteAudio { get; set; }
+
+    /// <summary>
+    /// Change the volume of a different sound
+    /// </summary>
+    /// <param name="slider">the Slider</param>
+    public void SetVolume(Slider slider)
+    {
+
+    }
+
+    #endregion
+}
